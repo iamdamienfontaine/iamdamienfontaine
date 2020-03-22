52f080d16f1a417669db7356b7d6524f79582385
Damien FONTAINE <279530@supinfo.com>
2020-03-22 14:25:44 +0100

    First commit 2 :)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
 import React from 'react';
 import profilImage from './img/profileImage.png'
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+	super(props);
+	this.state = {
+  	name: '',
+  	email: '',
+  	message: ''
+	  }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    axios({
+      method: "POST", 
+      url:"https://immense-mountain-06199.herokuapp.com/send", 
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success'){
+        alert("Message Sent."); 
+        this.resetForm()
+      }else if(response.data.status === 'fail'){
+        alert("Message failed to send.")
+      }
+    })
+  }
+
+  resetForm(){
+    
+     this.setState({name: '', email: '', message: ''})
+  }
+
+render() {
+ return(
     <div className="App">
       <h1>Mon site internet est encore en développement !</h1>  
       <h2>Auteur : Damien Fontaine - étudiant en informatique </h2>
-      <p>Salut anne ! nous avons un problème avec le coro anne virus !!</p>
       <img src={profilImage} alt="profil" />
+      <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+        <div className="form-group">
+            <label htmlFor="name">Nom</label>
+            <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email</label>
+            <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="message"></label>
+            <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+        </div>
+        <button type="submit" className="btn btn-primary">Envoyer</button>
+        </form>
     </div>
-  );
+ );
+}
+
+  onNameChange(event) {
+	this.setState({name: event.target.value})
+  }
+
+  onEmailChange(event) {
+	this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) {
+	this.setState({message: event.target.value})
+  }
 }
 
 export default App;
